649752c7b5e37aa0da5a23f66fd02a350c8e7c67
Kohei doi <tarutaru.pyorne@gmail.com>
2022-04-20 07:01:16 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/doi-training/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_grouop_name = "${var.prefix}-workshop"
+  resource_group_name = "${var.prefix}-workshop"
 }
